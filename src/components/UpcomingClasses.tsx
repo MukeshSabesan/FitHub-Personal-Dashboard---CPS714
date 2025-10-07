a0f504d6832a15f4815c2d319e203ab6aef6ddf1
Mukesh Sabesan <mukesh.sabesan@gmail.com>
2025-10-07 18:55:35 -0400

    Implemented JL changes with some modifications

--- src/components/UpcomingClasses.tsx
+++ src/components/UpcomingClasses.tsx
@@ -1,25 +1,28 @@
 function UpcomingClasses() {
 
-    const schedule = [
-        'Thursday Oct 2nd, 2-4pm, Yoga Class, Instructor: Sai Yogesh',
-        'Friday Oct 10th, 5-6pm, Intro to Kickboxing, Instructor: Alex Pereira',
-        'Wednesday Oct 11th, 5-6pm, Zumba, Instructor: Alejandra Garicia',
-        'Monday Oct 20th, 4-7pm, Basketball Tournament, Organizer: LeBron James',
-        'Friday Nov 1st, 2-4pm, Mental Health & Fitness, Instructor: James David'
+    const schedule = [ /* changed it so the schedule is separated by date, classname and instructor"*/
+      { date: "Thursday Oct 2nd, 2–4pm", className: "Yoga Class", instructor: "Sai Yogesh" },
+      { date: "Friday Oct 10th, 5–6pm", className: "Intro to Kickboxing", instructor: "Alex Pereira" },
+      { date: "Wednesday Oct 11th, 5–6pm", className: "Zumba", instructor: "Alejandra Garcia" },
+      { date: "Monday Oct 20th, 4–7pm", className: "Basketball Tournament", instructor: "LeBron James" },
+      { date: "Friday Nov 1st, 2–4pm", className: "Mental Health & Fitness", instructor: "James David" },
     ];
 
-    return (
-         <>
-      <h1>Upcoming Classes:</h1>
-      <ul className="list-group">
-        {schedule.map((item, index) => (
-          <li key={item} className="list-group-item">
-            <label htmlFor={`schedule-${index}`}>{item}</label>
-          </li>
-        ))}
-      </ul>     
-    </>
-    );
+  return (
+  <>
+    <h1>Upcoming Classes:</h1>
+    <ul className="list-group">
+      {schedule.map((item, index) => (
+        <li key={index} className="list-group-item schedule-item">
+          <div className="schedule-date">{item.date}</div>
+          <div className="schedule-class">{item.className}</div>
+          <div className="schedule-instructor">Instructor: {item.instructor}</div>
+        </li>
+      ))}
+    </ul>
+  </>
+  );
+
 }
 
 export default UpcomingClasses;
